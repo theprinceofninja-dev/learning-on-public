f790d83a5aaf0a5219016d20efc0e2e0bc3545a7
Mostafa <mostafa@cellusys.com>
2024-05-01 12:06:58 +0300

    project: keyboard presser

--- Projects/keyboard_presser.py
+++ Projects/keyboard_presser.py
@@ -1,14 +1,35 @@
-import time
+# import time
 
-import pyautogui
+# import pyautogui
 
-# To use this script, Run it and go to you telegram web app
-# Click on the Message area. and leave it :)
+# # To use this script, Run it and go to you telegram web app
+# # Click on the Message area. and leave it :)
 
-time.sleep(10)
-for i in range(51):
-    for c in f"Hello Houssam from automated script {i}":
-        pyautogui.press(c)
-    pyautogui.click()
-    pyautogui.press("Enter")
-    time.sleep(5)
+# time.sleep(5)
+# for i in range(51):
+#     for (
+#         c
+#     ) in f"h.f.j.f.G.h.f.k.j.h.G.f.d.h.f.j.f.G.h.f.k.j.h.G.f.d.f.h.f.j.f.G.h.f.k.j.h.G.f.d.h.f.j.f.G.h.f.k.j.h.G.f.d.f.h.f.j.f.G.h.f.k.j.h.G.f.d.h.f.j.f.G.h.f.k.j.h.G.f.d.f.h.f.j.f.G.h.f.k.j.h.G.f.d.h.f.j.f.G.h.f.k.j.h.G.f.d.f":
+#         pyautogui.press(c)
+#     pyautogui.click()
+
+#     pyautogui.press("Enter")
+#     time.sleep(5)
+
+import re
+
+
+def __validate_db_name(value) -> bool:
+    """
+    >>> __validate_db_name("test")
+    True
+    >>> __validate_db_name("test12")
+    True
+    >>> __validate_db_name("sql -- injection")
+    False
+    >>> __validate_db_name("test.test")
+    False
+    >>> __validate_db_name("test.test")
+    False
+    """
+    return bool(re.match("^[^0-9][a-zA-Z0-9_]{2,62}$", value))
